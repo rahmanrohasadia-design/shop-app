e0056d194a66269b0c2d4c3b5c7cddb10bece142
rahmanrohasadia-design <rahmanrohasadia@gmail.com>
2025-10-18 23:49:29 +0600

    my project

--- lib/pages/product_details_page.dart
+++ lib/pages/product_details_page.dart
@@ -62,86 +62,89 @@ class _ProductDetailsPageState extends State<ProductDetailsPage> {
           ),
           const Spacer(flex: 2),
           Container(
-            height: 280,
+            height: 225,
             decoration: BoxDecoration(
               color: const Color.fromRGBO(245, 247, 249, 1),
               borderRadius: BorderRadius.circular(40),
             ),
-child: Column(
-  crossAxisAlignment: CrossAxisAlignment.start, 
-  children: [ 
-    
-    Center(
-      child: Text(
-        '৳${widget.product['price']}',
-        style: const TextStyle(
-          fontSize: 22,
-          fontWeight: FontWeight.bold,
+child: SingleChildScrollView(
+  child: Column(
+    crossAxisAlignment: CrossAxisAlignment.start, 
+    children: [ 
+      
+      Center(
+        child: Text(
+          '৳${widget.product['price']}',
+          style: const TextStyle(
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+          ),
         ),
       ),
-    ),
-    const SizedBox(height: 15),
-
-   Padding(
-      padding: const EdgeInsets.only(left: 12), 
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            'RamRom: [${ramRom[0]}] [${ramRom[1]}]',
-            style: const TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
+      const SizedBox(height: 15),
+  
+     Padding(
+        padding: const EdgeInsets.only(left: 12), 
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'RamRom: [${ramRom[0]}] [${ramRom[1]}]',
+              style: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
-          const SizedBox(height: 8),
-
-          Text(
-            'Company: ${widget.product['Company']}',
-            style: const TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
+            const SizedBox(height: 7),
+  
+            Text(
+              'Company: ${widget.product['Company']}',
+              style: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
-          const SizedBox(height: 8),
-
-          Text(
-            'Camera: ${widget.product['Camera']}',
-            style: const TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
+            const SizedBox(height: 7),
+  
+            Text(
+              'Camera: ${widget.product['Camera']}',
+              style: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
-          const SizedBox(height: 8),
-
-          Text(
-            'Display: ${widget.product['Display']}',
-            style: const TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
+            const SizedBox(height: 7),
+  
+            Text(
+              'Display: ${widget.product['Display']}',
+              style: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+        ),
       ),
-    ),
-  ],
-),
-                ),
-                
-                 Padding(
-                   padding: const EdgeInsets.all(20.0),
-                   child: ElevatedButton(
-                    onPressed: onTap,
-                    style: ElevatedButton.styleFrom(
-                    backgroundColor: Theme.of(context).colorScheme.primary,
-                    fixedSize: const Size(350, 50),
-                    ),
-                   child: const Text('Add To Cart',
-                   style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 18,
-                   ),)
+    ],
+  ),
+                  ),
+                  
+                   const SizedBox(height: 7),
+                    Center(
+                     child: ElevatedButton(
+                      onPressed: onTap,
+                      style: ElevatedButton.styleFrom(
+                      backgroundColor: Theme.of(context).colorScheme.primary,
+                      fixedSize: const Size(300, 38),
+                      ),
+                     child: const Text('Add To Cart',
+                     style: TextStyle(
+                      color: Colors.black,
+                      
+                      fontSize: 18,
+                     ),)
+                     ),
                    ),
-                 ),
-              ],
-            )
+                ],
+              ),
+)
          ),
         ]
       )
